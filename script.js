3d09ca87a6292ea7a8fe506c91a38ab58de53844
Songhee Noh <s0n9h2@gmail.com>
2025-05-20 13:29:10 +0900

    초기화 버튼 추가

--- script.js
+++ script.js
@@ -10,10 +10,16 @@ function appendToHistory(text) {
   const li = document.createElement('li');
   li.textContent = text;
   document.getElementById('historyList').appendChild(li);
-}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+}
+
+function resetHistory() {
+  const list = document.getElementById('historyList');
+  list.innerHTML = '';
+}
 
 if (typeof module !== 'undefined') {
   module.exports = {
     appendToHistory,
+    resetHistory
    };
 }
